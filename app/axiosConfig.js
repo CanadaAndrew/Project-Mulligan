55b07c336479e170ccef4982d1a381c17d18b08e
VoicelessBark44 <34280572+VoicelessBark44@users.noreply.github.com>
2024-04-19 14:54:01 -0700

    DT-219
    Fixed issue with requiring log in to be pressed twice to log in

--- app/axiosConfig.js
+++ app/axiosConfig.js
@@ -7,10 +7,10 @@ const serverUrl = process.env.SERVER_URL;
 const database = axios.create({
     //baseURL: serverUrl,
     //baseURL: 'http://10.0.0.119:3000',  // Wilson local
-    baseURL: 'http://10.0.0.192:3000',
+    //baseURL: 'http://10.0.0.192:3000',
     //baseURL: 'http://192.168.1.150:3000', //Chris pc local
     //baseURL: 'http://10.0.0.14:3000', //Cameron Local
-    //baseURL: 'http://10.0.0.112:3000',
+    baseURL: 'http://10.0.0.112:3000',
     //baseURL: 'http://192.168.1.33:3000' //dru pc local
 });
 
