ed2d3428088f4abbcf37e7c4a6a188edb46640f5
cdub-616 <cdub616@aol.com>
2024-04-19 19:47:33 -0700

    fixed server connection bug

--- app/axiosConfig.js
+++ app/axiosConfig.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
-
-// Retrieve server URL from the .env file
-const serverUrl = process.env.SERVER_URL;
+import config from './config'; 
 
 // Create an instance of axios with the configured base URL
 const database = axios.create({
-    //baseURL: serverUrl,
+    baseURL: config.apiUrl,
     //baseURL: 'http://10.0.0.119:3000',  // Wilson local
-    baseURL: 'http://10.0.0.192:3000',
+    //baseURL: 'http://10.0.0.192:3000',
     //baseURL: 'http://192.168.1.150:3000', //Chris pc local
     //baseURL: 'http://10.0.0.14:3000', //Cameron Local
     //baseURL: 'http://10.0.0.112:3000',
     //baseURL: 'http://192.168.1.33:3000' //dru pc local
+    //baseURL: 'http://hair-done-wright530.azurewebsites.net'
 });
 
 export default database;
