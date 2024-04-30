91e182ab4027e42152572ff75ac20cd66c9aecd7
cdub-616 <cdub616@aol.com>
2024-04-29 22:46:20 -0700

    Update SignUp.tsx

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -116,7 +116,7 @@ export default function SignUp() { // added route for page navigation
         }
     }
     const setPhoneNumFormat = (input) => {
-        if( input.length <= 14){
+        if( input.length <= 12){
             if( /^\d*$/.test(input)){
                 const formatPhoNum = formattingPhoneNumber(input);
                 newPhoneNumber(formatPhoNum);
