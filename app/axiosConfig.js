9f5b836478e1d5efef37db6bbe132e1660ba9064
cdub-616 <cdub616@aol.com>
2024-05-09 18:56:30 -0700

    added delete modal to home screen

--- app/axiosConfig.js
+++ app/axiosConfig.js
@@ -3,7 +3,7 @@ import config from './config';
 
 // Create an instance of axios with the configured base URL
 const database = axios.create({
-    //baseURL: config.apiUrl,
+    baseURL: config.apiUrl,
     //baseURL: 'http://10.0.0.119:3000',  // Wilson local
     //baseURL: 'http://10.0.0.192:3000',
     //baseURL: 'http://192.168.1.150:3000', //Chris pc local
@@ -12,7 +12,7 @@ const database = axios.create({
     //baseURL: 'http://192.168.1.33:3000' //dru pc local
     //baseURL: 'http://hair-done-wright530.azurewebsites.net'
     //baseURL: 'http://192.168.68.131:3000' // tai laptop
-    baseURL: 'http://10.0.0.135:3000',
+    //baseURL: 'http://10.0.0.135:3000',
 
 });
 
