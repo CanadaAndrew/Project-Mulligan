29e4c1a6708ab31dd5e2de172ebbc812b28a4711
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2024-12-15 07:48:24 -0800

    cleaned up shared.tsx

--- app/shared.tsx
+++ app/shared.tsx
@@ -23,107 +23,47 @@ import { format, subDays, addDays, addMonths, subMonths, parseISO } from 'date-f
     output:  valid last date of month*/
 
 export function getValidDateDayBack(currentDate, daysBack) {
-  // Convert the current date to a Date object
   const date = parseISO(currentDate);
-
-  // Subtract the required days
   const startDate = subDays(date, daysBack);
-
-  // Format both dates to "YYYY-MM-DD" for react-native-calendars
-  const formattedStartDate = format(startDate, 'yyyy-MM-dd');
-  //const formattedEndDate = format(date, 'yyyy-MM-dd');
-  return formattedStartDate;
-  /*return {
-    //startDate: formattedStartDate,
-    //endDate: formattedEndDate,
-  };*/
+  return format(startDate, 'yyyy-MM-dd');
 }
 
 export function getFutureDateByDay(currentDate, daysForward) {
-  // Convert the current date to a Date object
   const date = parseISO(currentDate);
-  
-  // Add the required days
   const endDate = addDays(date, daysForward);
-  
-  // Format both dates to "YYYY-MM-DD" for react-native-calendars
-  //const formattedStartDate = format(date, 'yyyy-MM-dd');
-  const formattedEndDate = format(endDate, 'yyyy-MM-dd');
-  return formattedEndDate;
-  /*return {
-    startDate: formattedStartDate,
-    endDate: formattedEndDate,
-  };*/
+  return format(endDate, 'yyyy-MM-dd');
 }
 
 export function getValidDateByWeeks(currentDate, weeksBack) {
-    // Convert weeks to days
     const daysBack = weeksBack * 7;
-  
     const date = parseISO(currentDate);
     const startDate = subDays(date, daysBack);
-  
-    const formattedStartDate = format(startDate, 'yyyy-MM-dd');
-    //const formattedEndDate = format(date, 'yyyy-MM-dd');
-    return formattedStartDate;
-    /*return {
-      startDate: formattedStartDate,
-      //endDate: formattedEndDate,
-    };*/
+    return format(startDate, 'yyyy-MM-dd');
   }
   
 export function getFutureDateByWeeks(currentDate, weeksForward) {
-  // Convert weeks to days
   const daysForward = weeksForward * 7;
-  
   const date = parseISO(currentDate);
   const endDate = addDays(date, daysForward);
-  
-  //const formattedStartDate = format(date, 'yyyy-MM-dd');
-  const formattedEndDate = format(endDate, 'yyyy-MM-dd');
-  return formattedEndDate;
-  /*return {
-    startDate: formattedStartDate,
-    //endDate: formattedEndDate,
-  };*/
+  return format(endDate, 'yyyy-MM-dd');
 }
 
 export function getValidDateByMonths(currentDate, monthsBack) {
   const date = parseISO(currentDate);
   const startDate = subMonths(date, monthsBack);
-  
-  const formattedStartDate = format(startDate, 'yyyy-MM-dd');
-  //const formattedEndDate = format(date, 'yyyy-MM-dd');
-  return formattedStartDate;
-  /*return {
-    startDate: formattedStartDate,
-    //endDate: formattedEndDate,
-  };*/
+  return format(startDate, 'yyyy-MM-dd');
 }
   
 export function getFutureDateByMonths(currentDate, monthsForward) {
   const date = parseISO(currentDate);
   const endDate = addMonths(date, monthsForward);
-  
-  //const formattedStartDate = format(date, 'yyyy-MM-dd');
-  const formattedEndDate = format(endDate, 'yyyy-MM-dd');
-  return formattedEndDate;
-  /*return {
-    startDate: formattedStartDate,
-    //endDate: formattedEndDate,
-  };*/
+  return format(endDate, 'yyyy-MM-dd');
 }
 
 export function getLastDateOfMonth(currentDate) {
   const date = parseISO(currentDate);
-
-  // Get the year and month
   const year = date.getFullYear();
   const month = date.getMonth();
-
-  // Create a date for the first day of the next month, then subtract 1 day
-  const lastDay = new Date(year, month + 1, 0); // Day 0 = last day of the current month
-
-  // Return the date in 'YYYY-MM-DD' format
+  const lastDay = new Date(year, month + 1, 0); // Day 0 = last day of current month
   return lastDay.toISOString().split("T")[0];
 }
